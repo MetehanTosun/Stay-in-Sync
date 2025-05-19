77c0eb744f977b70da4ada7eb877b7db6ad03821
Alexander Feufel <st187824@stud.uni-stuttgart.de>
2025-05-19 14:32:26 +0200

    folderstructur

---
+++ stay-in-sync-monitoring/monitoring-ui/src/app/dashboard/graph-panel/graph-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-graph-panel',
+  imports: [],
+  templateUrl: './graph-panel.component.html',
+  styleUrl: './graph-panel.component.css'
+})
+export class GraphPanelComponent {
+
+}
---
+++ stay-in-sync-monitoring/monitoring-ui/src/app/dashboard/logs-panel/logs-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logs-panel',
+  imports: [],
+  templateUrl: './logs-panel.component.html',
+  styleUrl: './logs-panel.component.css'
+})
+export class LogsPanelComponent {
+
+}
