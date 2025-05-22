b6eab7b620220932c50b1876a09a820ec0ce18e7
st187824 <st187824@stud.uni-stuttgart.de>
2025-05-22 12:32:09 +0200

    Fix(Config-Ui): imports removed

--- stay-in-sync-configurator-ui/src/app/sidebar-menu/sidebar-menu.component.ts
+++ stay-in-sync-configurator-ui/src/app/sidebar-menu/sidebar-menu.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 import {Sidebar} from 'primeng/sidebar';
-import {Avatar} from 'primeng/avatar';
 import {Button} from 'primeng/button';
-import {StyleClass} from 'primeng/styleclass';
-import {PrimeTemplate} from 'primeng/api';
-import {Ripple} from 'primeng/ripple';
 
 @Component({
   selector: 'app-sidebar-menu',
-  imports: [Sidebar, Avatar, Button, StyleClass, PrimeTemplate, Ripple],
+  imports: [Sidebar, Button],
   templateUrl: './sidebar-menu.component.html',
   styleUrl: './sidebar-menu.component.css'
 })
@@ -16,7 +12,4 @@ export class SidebarMenuComponent {
 
   sidebarVisible: boolean = true;
 
-  closeCallback($event: MouseEvent) {
-
-  }
 }
