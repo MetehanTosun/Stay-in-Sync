5ffa41573965546364322412a96aabefc239d33a
st187824 <st187824@stud.uni-stuttgart.de>
2025-05-28 21:59:36 +0200

    Feat(MonitoringUI): searchbar component, that can filter the Graph, comments and refactoring in Graph component

--- stay-in-sync-monitoring/monitoring-ui/src/app/dashboard/search-bar/search-bar.component.ts
+++ stay-in-sync-monitoring/monitoring-ui/src/app/dashboard/search-bar/search-bar.component.ts
@@ -10,7 +10,13 @@ export class SearchBarComponent {
   @Output() search = new EventEmitter<string>();
 
   onInput(event: Event) {
+    console.log('Search input changed:', event);
     const value = (event.target as HTMLInputElement).value;
+    this.search.emit(value);
+  }
+
+  onSearch(value: string) {
+    console.log('Search button clicked:', value);
     this.search.emit(value);
   }
 }
