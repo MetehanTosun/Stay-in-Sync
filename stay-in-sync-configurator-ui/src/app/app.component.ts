48bf0e098f9889747985d469f078e77c85e2b11b
st187824 <st187824@stud.uni-stuttgart.de>
2025-06-02 13:39:24 +0200

    Fix(ConfiguratorUI): ASS und EDC Überschriftft

--- stay-in-sync-configurator-ui/src/app/app.component.ts
+++ stay-in-sync-configurator-ui/src/app/app.component.ts
@@ -1,9 +1,7 @@
 import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {SidebarMenuComponent} from './features/sidebar-menu/sidebar-menu.component';
-import {MessageModule} from 'primeng/message';
 import {ToastModule} from 'primeng/toast';
-import {SyncJobPageComponent} from './features/sync-job/components/sync-job-page/sync-job-page.component';
 import {NgIf} from '@angular/common';
 
 
@@ -15,15 +13,10 @@ import {NgIf} from '@angular/common';
 })
 
 export class AppComponent {
-  /** Steuert, ob die Sidebar angezeigt wird */
+
   sidebarVisible = true;
 
-  /**
-   * Optional: Methode zur Reaktion auf Änderungen.
-   * Diese könnte z. B. Logging oder weitere UI-Updates auslösen.
-   */
   onSidebarToggle(visible: boolean): void {
     this.sidebarVisible = visible;
-    console.log('Sidebar Sichtbarkeit geändert:', visible);
   }
 }
