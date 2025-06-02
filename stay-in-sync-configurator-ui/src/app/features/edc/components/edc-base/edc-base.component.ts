f2f50726de1467051dd012e08c7d6aff5f1edfd1
YardenKrupik <145471212+YardenKrupik@users.noreply.github.com>
2025-06-02 19:18:47 +0200

    main sidebar

--- stay-in-sync-configurator-ui/src/app/features/edc/components/edc-base/edc-base.component.ts
+++ stay-in-sync-configurator-ui/src/app/features/edc/components/edc-base/edc-base.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-edc-base',
-  imports: [],
   templateUrl: './edc-base.component.html',
-  styleUrl: './edc-base.component.css'
+  styleUrls: ['./edc-base.component.css']
 })
 export class EdcBaseComponent {
-
+  activeTab: string = 'tab1';
 }
