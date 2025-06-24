c151598df63eccc86760bbfec892234d534ac5f0
spicysesam <st187719@stud.uni-stuttgart.de>
2025-06-24 17:20:03 +0200

    PollingJobConfigurator implemented
    Implementation finished and tests passed

---
+++ stay-in-sync-core/core-polling-node/src/main/de/unistuttgart/stayinsync/pollingnode/usercontrol/configuration/PollingJobConfigurator.java
@@ -0,0 +1,32 @@
+package de.unistuttgart.stayinsync.pollingnode.usercontrol.configuration;
+
+import de.unistuttgart.stayinsync.pollingnode.entities.PollingJob;
+import de.unistuttgart.stayinsync.pollingnode.entities.SyncJob;
+import de.unistuttgart.stayinsync.pollingnode.exceptions.FaultySyncJobException;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+
+/**
+ * Creates PollingJobs out of SyncJobs with needed fields.
+ */
+@ApplicationScoped
+public class PollingJobConfigurator {
+
+    public PollingJobConfigurator(){
+        super();
+    }
+
+    /**
+     * Creates PollingJob based on information of SyncJob
+     * @param syncJob used to get important initialisation information
+     */
+    public PollingJob createPollingJob(final SyncJob syncJob) throws FaultySyncJobException{
+        final String apiAddress = syncJob.getApiAddress();
+         if(apiAddress == null || apiAddress.isEmpty()){
+             throw new FaultySyncJobException("SyncJob did not have defined source systems.");
+        }
+        return new PollingJob(apiAddress);
+    }
+
+}
