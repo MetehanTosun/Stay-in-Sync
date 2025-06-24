d372c7ad57026bf204b2d9a21adf151df5fbecf7
spicysesam <st187719@stud.uni-stuttgart.de>
2025-06-24 17:10:23 +0200

    PollingJob updated

---
+++ stay-in-sync-core/core-polling-node/src/main/de/unistuttgart/stayinsync/pollingnode/entities/PollingJob.java
@@ -0,0 +1,17 @@
+package de.unistuttgart.stayinsync.pollingnode.entities;
+
+public class PollingJob {
+
+    private final String apiAddress;
+
+    public PollingJob(final String apiAddress){
+        this.apiAddress = apiAddress;
+    }
+
+
+    public String getApiAddress() {
+        return apiAddress;
+    }
+
+
+}
