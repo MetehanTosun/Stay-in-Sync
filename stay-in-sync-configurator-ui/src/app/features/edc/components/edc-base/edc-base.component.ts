6633dd91e65ef4088a3963e998eac91af51f81eb
YardenKrupik <145471212+YardenKrupik@users.noreply.github.com>
2025-07-05 03:24:24 +0200

    sidebar design

--- stay-in-sync-configurator-ui/src/app/features/edc/components/edc-base/edc-base.component.ts
+++ stay-in-sync-configurator-ui/src/app/features/edc/components/edc-base/edc-base.component.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 import { EdcInstancesComponent } from '../edc-instances/edc-instances.component';
 import { EdcAssetsAndPoliciesComponent } from '../edc-assets-and-policies/edc-assets-and-policies.component';
 
+type EdcTab = 'instances' | 'assets and policies';
+
 @Component({
   selector: 'app-edc-base',
   standalone: true,
   imports: [CommonModule, EdcInstancesComponent, EdcAssetsAndPoliciesComponent],
   templateUrl: './edc-base.component.html',
-  styleUrls: ['./edc-base.component.css']
+  styleUrls: ['./edc-base.component.css'],
 })
 export class EdcBaseComponent {
-  activeTab: string = 'instances';
+
+  activeTab: EdcTab = 'instances';
+
+  setActiveTab(tab: EdcTab): void {
+    this.activeTab = tab;
+  }
 }
