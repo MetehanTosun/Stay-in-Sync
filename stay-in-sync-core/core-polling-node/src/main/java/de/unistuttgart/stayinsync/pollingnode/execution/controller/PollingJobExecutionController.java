4f336e4fd56fc69e19765701221db175ce8fed27
spicysesam <st187719@stud.uni-stuttgart.de>
2025-07-08 17:57:10 +0200

    Updated PollingJobExecutionController to be compatible with reworked restClient and support pollingTiming

--- stay-in-sync-core/core-polling-node/src/main/java/de/unistuttgart/stayinsync/pollingnode/execution/controller/PollingJobExecutionController.java
+++ stay-in-sync-core/core-polling-node/src/main/java/de/unistuttgart/stayinsync/pollingnode/execution/controller/PollingJobExecutionController.java
@@ -1,69 +1,105 @@
 package de.unistuttgart.stayinsync.pollingnode.execution.controller;
 
+import de.unistuttgart.stayinsync.polling.exception.PollingNodeException;
+import de.unistuttgart.stayinsync.pollingnode.entities.ApiConnectionDetails;
 import de.unistuttgart.stayinsync.pollingnode.entities.PollingJob;
-import de.unistuttgart.stayinsync.pollingnode.execution.service.PollingJobPollingService;
+import de.unistuttgart.stayinsync.pollingnode.exceptions.FaultySourceSystemApiRequestMessageDtoException;
+import de.unistuttgart.stayinsync.pollingnode.exceptions.JsonObjectUnthreadingException;
+import de.unistuttgart.stayinsync.pollingnode.execution.ressource.RestClient;
 
+import de.unistuttgart.stayinsync.transport.dto.SourceSystemApiRequestConfigurationMessageDTO;
+import io.quarkus.logging.Log;
+import io.smallrye.mutiny.Uni;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
+import io.vertx.mutiny.core.buffer.Buffer;
 import jakarta.enterprise.context.ApplicationScoped;
 
 import de.unistuttgart.stayinsync.pollingnode.rabbitmq.ProducerSendPolledData;
+
+import java.net.http.HttpRequest;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
+import java.util.Objects;
+import java.util.concurrent.*;
 
 @ApplicationScoped
 public class PollingJobExecutionController {
 
-    private final PollingJobPollingService pollingJobPollingService;
     private final ProducerSendPolledData producerSendPolledData;
-    private final Map<String, PollingJob> supportedPollingJobs;
-    private final Map<PollingJob, ScheduledFuture<?>> openPollingJobThreads;
+    private final RestClient restClient;
     private final ScheduledExecutorService jobExecutor;
+    private final Map<Long, ScheduledFuture<?>> openPollingJobThreads;
 
-    public PollingJobExecutionController(final PollingJobPollingService pollingJobPollingService, final ProducerSendPolledData producerSendPolledData){
+    public PollingJobExecutionController( final ProducerSendPolledData producerSendPolledData, final RestClient restClient){
         super();
-        this.pollingJobPollingService = pollingJobPollingService;
         this.producerSendPolledData = producerSendPolledData;
-        this.supportedPollingJobs = new HashMap<>();
+        this.restClient = restClient;
         this.openPollingJobThreads= new HashMap<>();
         this. jobExecutor = Executors.newScheduledThreadPool(10);
+
     }
 
-    public void startPollingJobExecution(final PollingJob pollingJob){
-        supportedPollingJobs.put(pollingJob.getApiAddress(),pollingJob);
-        timedPollingJobThreadActivation(pollingJob);
+    public void startPollingJobExecution(final SourceSystemApiRequestConfigurationMessageDTO apiRequestConfigurationMessage) throws FaultySourceSystemApiRequestMessageDtoException{
+        timedPollingJobThreadActivation(apiRequestConfigurationMessage);
     }
 
-    public void stopPollingJobExecution(final String apiAddress){
-        timedPollingJobThreadDeletion(supportedPollingJobs.remove(apiAddress));
-        supportedPollingJobs.remove(apiAddress);
+    public void stopPollingJobExecution(final Long id){
+        timedPollingJobThreadDeletion(id);
+        openPollingJobThreads.remove(id);
     }
 
-    public boolean pollingJobExists(final String apiAddress){
-        return supportedPollingJobs.containsKey(apiAddress);
+    public boolean pollingJobExists(final Long id){
+        return openPollingJobThreads.containsKey(id);
     }
 
-    private void timedPollingJobThreadActivation(final PollingJob pollingJob) {
+    private void timedPollingJobThreadActivation(final SourceSystemApiRequestConfigurationMessageDTO apiRequestConfigurationMessage) throws FaultySourceSystemApiRequestMessageDtoException {
+        if(Objects.equals(apiRequestConfigurationMessage.sourceSystem().apiType(), "No Spec") && !Objects.equals(apiRequestConfigurationMessage.endpoint().httpRequestType(), "GET")){
+            throw new FaultySourceSystemApiRequestMessageDtoException("Use of PUT or POST is not possible on No Spec API´s");
+        }
         Runnable task = () -> {
-            producerSendPolledData.send(pollingJobPollingService.pollAndMapData(pollingJob.getApiAddress()));
+            try {
+                JsonObject jsonObject = unpackUniJsonObject(pollUniJsonObject(apiRequestConfigurationMessage));
+                Log.infof("JsonObject polled successfully: %s", jsonObject);
+                //TODO JsonObject wird für noch nichts genutzt
+            }catch(FaultySourceSystemApiRequestMessageDtoException | JsonObjectUnthreadingException e){
+                Log.error(e);
+            }
         };
 
         ScheduledFuture<?> future = jobExecutor.scheduleAtFixedRate(
                 task,
                 0,
-                1000,
+                apiRequestConfigurationMessage.pollingIntervallTimeInMs(),
                 TimeUnit.MILLISECONDS
         );
 
-        openPollingJobThreads.put(pollingJob, future);
+        openPollingJobThreads.put(apiRequestConfigurationMessage.id(), future);
     }
 
-    private void timedPollingJobThreadDeletion(final PollingJob pollingJob) {
-        ScheduledFuture<?> future = openPollingJobThreads.remove(pollingJob);
+    private void timedPollingJobThreadDeletion(final Long id) {
+        ScheduledFuture<?> future = openPollingJobThreads.remove(id);
         if (future != null) {
             future.cancel(true);
         }
     }
+
+    private Uni<JsonObject> pollUniJsonObject(final SourceSystemApiRequestConfigurationMessageDTO apiRequestConfigurationMessage) throws FaultySourceSystemApiRequestMessageDtoException {
+        return restClient.executeRequest(restClient.configureRequest(apiRequestConfigurationMessage));
+    }
+
+    private JsonObject unpackUniJsonObject(final Uni<JsonObject> uniJsonObject) throws JsonObjectUnthreadingException {
+        try{
+            return uniJsonObject
+                    .subscribe().asCompletionStage()
+                    .toCompletableFuture()
+                    .get();
+        } catch(InterruptedException e){
+            throw new JsonObjectUnthreadingException("The unpacking of CompletableFuture to obtain JsonObject was interrupted with this exception message: " + e);
+        } catch(ExecutionException e){
+            throw new JsonObjectUnthreadingException("The unpacking of CompletableFuture to obtain JsonObject was interrupted by an unforseen exception in the api call process: " + e);
+        }
+    }
+
+
 }
