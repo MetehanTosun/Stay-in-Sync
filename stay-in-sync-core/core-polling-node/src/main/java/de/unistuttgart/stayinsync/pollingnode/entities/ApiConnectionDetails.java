c618b2ca8a896265b2dc99e3f4b850264ad694c2
spicysesam <st187719@stud.uni-stuttgart.de>
2025-07-08 17:54:26 +0200

    ApiConnectionDetails removed

--- stay-in-sync-core/core-polling-node/src/main/java/de/unistuttgart/stayinsync/pollingnode/entities/ApiConnectionDetails.java
+++ stay-in-sync-core/core-polling-node/src/main/java/de/unistuttgart/stayinsync/pollingnode/entities/ApiConnectionDetails.java
@@ -11,33 +11,6 @@ import java.util.Set;
 public record ApiConnectionDetails(SourceSystemMessageDTO sourceSystem, SourceSystemEndpointMessageDTO endpoint,  Set<ApiRequestParameterMessageDTO> requestParameters,
                                    Set<ApiRequestHeaderMessageDTO> requestHeader) {
 
-    public boolean allFieldsInCorrectFormat(){
-        return allFieldsNotNull() && allImportantStringsNotEmpty();
-    }
 
-    private boolean allFieldsNotNull(){
-        if(sourceSystem == null
-                || sourceSystem.apiUrl() == null
-                || sourceSystem.apiType() == null
-                || sourceSystem.authDetails() == null
-                || sourceSystem.authDetails().apiKey == null
-                || sourceSystem.authDetails().headerName == null){
-            return false;
-        }
-        if(endpoint == null || endpoint.endpointPath() == null || endpoint.httpRequestType() == null){
-            return false;
-        }
-        if(requestParameters == null){
-            return false;
-        }
-        if(requestHeader == null){
-            return false;
-        }
-        return true;
-    }
-
-    private boolean allImportantStringsNotEmpty(){
-        return !Objects.equals(sourceSystem.apiUrl(), "") && !Objects.equals(endpoint.endpointPath(),"");
-    }
 
 }
