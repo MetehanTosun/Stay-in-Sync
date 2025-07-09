b99cb9116b4a6fd00229deff79f4d8277def94e7
Yasin Tüzen <yasin.tuezen@web.de>
2025-07-09 13:06:48 +0200

    FIx DTOs

--- stay-in-sync-core/core-management/src/main/java/de/unistuttgart/stayinsync/core/configuration/rest/dtos/SourceSystemEndpointDto.java
+++
@@ -1,7 +0,0 @@
-package de.unistuttgart.stayinsync.core.configuration.rest.dtos;
-
-import jakarta.validation.constraints.NotNull;
-
-public record SourceSystemEndpointDTO(Long id, Long sourceSystemId, @NotNull String endpointPath,
-                                      @NotNull String httpRequestType) {
-}
