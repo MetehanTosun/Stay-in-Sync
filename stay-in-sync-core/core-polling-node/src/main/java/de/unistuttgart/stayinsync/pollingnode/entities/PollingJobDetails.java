9b70af659e2f9f82f6f4abd5b742630e0fec2319
spicysesam <st187719@stud.uni-stuttgart.de>
2025-07-19 14:04:44 +0200

    Created two new Entities, which hold the same properties as SourceSystemApiRequestConfigurationMessageDTO and ApiConnectionDetailsDTO but will improve the readability of this components code by taking their place in future changes

---
+++ stay-in-sync-core/core-polling-node/src/main/java/de/unistuttgart/stayinsync/pollingnode/entities/PollingJobDetails.java
@@ -0,0 +1,5 @@
+package de.unistuttgart.stayinsync.pollingnode.entities;
+
+public record PollingJobDetails(String name, Long id, int pollingIntervallTimeInMs, boolean active,
+                                RequestBuildingDetails requestBuildingDetails) {
+}
