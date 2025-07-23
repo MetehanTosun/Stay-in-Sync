7a8c93e83948a967f6e04ec1d93cdf230948db8e
Yasin Tüzen <yasin.tuezen@web.de>
2025-07-23 23:32:56 +0200

    Add test cases

--- stay-in-sync-core/core-management/src/test/java/de/unistuttgart/stayinsync/persistence/entities/SourceSystemEndpointTest.java
+++ stay-in-sync-core/core-management/src/test/java/de/unistuttgart/stayinsync/persistence/entities/SourceSystemEndpointTest.java
@@ -51,4 +51,95 @@ public class SourceSystemEndpointTest {
         });
         Assertions.assertTrue(ex.getMessage().contains("responseBodySchema"));
     }
+
+    @Test
+    public void testPersistAndLoadComplexResponseBodySchema() {
+        SourceSystem sourceSystem = new SourceSystem();
+        sourceSystem.name = "ComplexSystem";
+        sourceSystem.apiUrl = "http://complex";
+        sourceSystem.apiType = "REST";
+        em.persist(sourceSystem);
+
+        String complexSchema = "{" +
+                "\"type\":\"object\"," +
+                "\"properties\":{\"foo\":{\"type\":\"string\"},\"bar\":{\"type\":\"array\",\"items\":{\"type\":\"integer\"}}}," +
+                "\"required\":[\"foo\"]" +
+                "}";
+
+        SourceSystemEndpoint endpoint = new SourceSystemEndpoint();
+        endpoint.endpointPath = "/complex";
+        endpoint.httpRequestType = "POST";
+        endpoint.responseBodySchema = complexSchema;
+        endpoint.sourceSystem = sourceSystem;
+        endpoint.syncSystem = sourceSystem;
+        em.persist(endpoint);
+        em.flush();
+        em.clear();
+
+        SourceSystemEndpoint loaded = em.find(SourceSystemEndpoint.class, endpoint.id);
+        Assertions.assertEquals(complexSchema, loaded.responseBodySchema);
+    }
+
+    @Test
+    public void testUpdateResponseBodySchema() {
+        SourceSystem sourceSystem = new SourceSystem();
+        sourceSystem.name = "UpdateSystem";
+        sourceSystem.apiUrl = "http://update";
+        sourceSystem.apiType = "REST";
+        em.persist(sourceSystem);
+
+        SourceSystemEndpoint endpoint = new SourceSystemEndpoint();
+        endpoint.endpointPath = "/update";
+        endpoint.httpRequestType = "PUT";
+        endpoint.responseBodySchema = "{\"type\":\"string\"}";
+        endpoint.sourceSystem = sourceSystem;
+        endpoint.syncSystem = sourceSystem;
+        em.persist(endpoint);
+        em.flush();
+        Long id = endpoint.id;
+        em.clear();
+
+        // Update
+        SourceSystemEndpoint toUpdate = em.find(SourceSystemEndpoint.class, id);
+        toUpdate.responseBodySchema = "{\"type\":\"number\"}";
+        em.flush();
+        em.clear();
+
+        SourceSystemEndpoint loaded = em.find(SourceSystemEndpoint.class, id);
+        Assertions.assertEquals("{\"type\":\"number\"}", loaded.responseBodySchema);
+    }
+
+    @Test
+    public void testMultipleEndpointsWithDifferentSchemas() {
+        SourceSystem sourceSystem = new SourceSystem();
+        sourceSystem.name = "MultiSystem";
+        sourceSystem.apiUrl = "http://multi";
+        sourceSystem.apiType = "REST";
+        em.persist(sourceSystem);
+
+        SourceSystemEndpoint ep1 = new SourceSystemEndpoint();
+        ep1.endpointPath = "/ep1";
+        ep1.httpRequestType = "GET";
+        ep1.responseBodySchema = "{\"type\":\"object\"}";
+        ep1.sourceSystem = sourceSystem;
+        ep1.syncSystem = sourceSystem;
+        em.persist(ep1);
+
+        SourceSystemEndpoint ep2 = new SourceSystemEndpoint();
+        ep2.endpointPath = "/ep2";
+        ep2.httpRequestType = "POST";
+        ep2.responseBodySchema = "{\"type\":\"array\"}";
+        ep2.sourceSystem = sourceSystem;
+        ep2.syncSystem = sourceSystem;
+        em.persist(ep2);
+
+        em.flush();
+        em.clear();
+
+        SourceSystemEndpoint loaded1 = em.find(SourceSystemEndpoint.class, ep1.id);
+        SourceSystemEndpoint loaded2 = em.find(SourceSystemEndpoint.class, ep2.id);
+        Assertions.assertEquals("{\"type\":\"object\"}", loaded1.responseBodySchema);
+        Assertions.assertEquals("{\"type\":\"array\"}", loaded2.responseBodySchema);
+    }
 }
+
