df28df4c9abcbb30695a555ea64580fc0cab1cd9
spicysesam <st187719@stud.uni-stuttgart.de>
2025-07-30 16:03:46 +0200

    Minor changes in these files

--- stay-in-sync-core/core-polling-node/src/main/java/de/unistuttgart/stayinsync/pollingnode/entities/PollingJobDetails.java
+++ stay-in-sync-core/core-polling-node/src/main/java/de/unistuttgart/stayinsync/pollingnode/entities/PollingJobDetails.java
@@ -2,4 +2,6 @@ package de.unistuttgart.stayinsync.pollingnode.entities;
 
 public record PollingJobDetails(String name, Long id, int pollingIntervallTimeInMs, boolean active,
                                 RequestBuildingDetails requestBuildingDetails) {
+
 }
+
--- stay-in-sync-core/core-polling-node/src/main/java/de/unistuttgart/stayinsync/pollingnode/execution/service/RequestConfigurationService.java
+++
@@ -1,4 +0,0 @@
-package de.unistuttgart.stayinsync.pollingnode.execution.service;
-
-public class RequestConfigurationService {
-}
