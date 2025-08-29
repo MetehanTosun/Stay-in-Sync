1ec2dd108b7355eb25224ff9d6e4968fcd4fafdf
spicysesam <st187719@stud.uni-stuttgart.de>
2025-08-29 18:14:42 +0200

    Whole AssetSystem was refactored. The classes are in an unfinished state and should be worked on by Tima

--- stay-in-sync-core/core-management/src/main/java/de/unistuttgart/stayinsync/core/configuration/edc/service/EDCAssetService.java
+++ stay-in-sync-core/core-management/src/main/java/de/unistuttgart/stayinsync/core/configuration/edc/service/EDCAssetService.java
@@ -1,8 +1,13 @@
 package de.unistuttgart.stayinsync.core.configuration.edc.service;
 
+import de.unistuttgart.stayinsync.core.configuration.edc.dtoedc.EDCAssetDto;
+import de.unistuttgart.stayinsync.core.configuration.edc.mapping.EDCAssetMapper;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.transaction.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -12,40 +17,92 @@ import de.unistuttgart.stayinsync.core.configuration.edc.entities.EDCAsset;
 @ApplicationScoped
 public class EDCAssetService {
 
-    public List<EDCAsset> listAll() {
-        return EDCAsset.listAll();
-    }
 
-    public Optional<EDCAsset> findById(UUID id) {
-        return EDCAsset.findByIdOptional(id);
-    }
-
-    @Transactional
-    public EDCAsset create(EDCAsset entity) {
-        entity.persist();
-        return entity;
-    }
-
-    @Transactional
-    public Optional<EDCAsset> update(UUID id, EDCAsset newState) {
-        EDCAsset existing = EDCAsset.findById(id);
-        if (existing == null) {
-            return Optional.empty();
+    /**
+     * Returns an asset found in the database with the id.
+     *
+     * @param id used to find the asset
+     * @return found asset
+     */
+    public EDCAssetDto findById(final UUID id) throws CustomException {
+        final EDCAsset asset = EDCAsset.findById(id);
+        if (asset == null) {
+            final String exceptionMessage = "No Asset found with id";
+            Log.error(exceptionMessage);
+            throw new CustomException(exceptionMessage); //TODO Erstelle eigene Exceptions für Exception Handling
         }
-        existing.assetId               = newState.assetId;
-        existing.url                   = newState.url;
-        existing.type                  = newState.type;
-        existing.contentType           = newState.contentType;
-        existing.description           = newState.description;
-        existing.dataAddress           = newState.dataAddress;
-        existing.properties            = newState.properties;
-        existing.targetSystemEndpoint  = newState.targetSystemEndpoint;
-        existing.targetEDC             = newState.targetEDC;
-        return Optional.of(existing);
+        return EDCAssetMapper.assetMapper.assetToAssetDto(EDCAsset.findById(id));
     }
 
+    /**
+     * Returns a list with all assets saved in the database.
+     *
+     * @return List with all assets
+     */
+    public List<EDCAssetDto> listAll() {
+        //TODO finde heraus warum hier die Assets in diesem Format sind: PanacheEntityBase Und fixe es.
+        List<EDCAssetDto> assets = new ArrayList<>();
+        for (PanacheEntityBase asset : EDCAsset.listAll()) {
+            EDCAssetMapper.assetMapper.assetToAssetDto(asset);
+        }
+
+        return assets;
+    }
+
+    /**
+     * Moves asset into the database and returns it to the caller.
+     *
+     * @param assetDto to be persisted.
+     * @return the created asset.
+     */
     @Transactional
-    public boolean delete(UUID id) {
+    public EDCAssetDto create(EDCAssetDto assetDto) {
+        EDCAsset asset = EDCAssetMapper.assetMapper.assetDtoToAsset(assetDto);
+        asset.persist();
+
+        return EDCAssetMapper.assetMapper.assetToAssetDto(asset);
+    }
+
+    /**
+     * Searches for database entry with id. The returned object is linked to the database.
+     * Database is updated according to changes after the program flow moves out of this method/transaction.
+     *
+     * @param id              to find the database entry
+     * @param updatedAssetDto contains the updated data
+     * @return Optional with the updated asset or an empty Optional if nothing was found.
+     */
+    @Transactional
+    public EDCAssetDto update(UUID id, EDCAssetDto updatedAssetDto) throws CustomException{
+        final EDCAsset persistedAsset = EDCAsset.findById(id);
+        final EDCAsset updatedAsset = EDCAssetMapper.assetMapper.assetDtoToAsset(updatedAssetDto);
+
+        if (persistedAsset == null) {
+            final String exceptionMessage = "No Asset found with id";
+            Log.errorf(exceptionMessage);
+            throw new CustomException(exceptionMessage); //TODO Erstelle eigene Exceptions für Exception Handling
+        }
+
+        persistedAsset.setAssetId(updatedAsset.getAssetId());
+        persistedAsset.setUrl(updatedAsset.getUrl());
+        persistedAsset.setType(updatedAsset.getType());
+        persistedAsset.setContentType(updatedAsset.getContentType());
+        persistedAsset.setDescription(updatedAsset.getDescription());
+        persistedAsset.setDataAddress(updatedAsset.getDataAddress());
+        persistedAsset.setProperties(updatedAsset.getProperties());
+        persistedAsset.setTargetSystemEndpoint(updatedAsset.getTargetSystemEndpoint());
+        persistedAsset.setTargetEDC(updatedAsset.getTargetEDC());
+
+        return EDCAssetMapper.assetMapper.assetToAssetDto(persistedAsset);
+    }
+
+    /**
+     * Removes the asset from the database
+     *
+     * @param id used to find asset to be deleted in database
+     * @return the deleted asset
+     */
+    @Transactional
+    public boolean delete(final UUID id) {
         return EDCAsset.deleteById(id);
     }
 }
