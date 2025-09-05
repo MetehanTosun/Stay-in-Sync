4ac4a1d2403f3ca28ef595f63a3416e8e92b31a8
Mohammed-Ammar Hassnou <st188386@stud.uni-stuttgart.de>
2025-09-05 19:09:05 +0200

    Update TransformJob.java
    test to see if the pipeline failed because of the extra attribute

--- stay-in-sync-core/core-sync-node/src/main/java/de/unistuttgart/stayinsync/syncnode/domain/TransformJob.java
+++ stay-in-sync-core/core-sync-node/src/main/java/de/unistuttgart/stayinsync/syncnode/domain/TransformJob.java
@@ -4,14 +4,13 @@ import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @RegisterForReflection
 public record TransformJob(
-        Long transformationId,
-        String name,
-        String jobId,
-        String scriptId,
-        String scriptCode,
-        String scriptLanguage,
-        String expectedHash,
-        String generatedSdkCode,
-        String generatedSdkHash,
-        Object sourceData
-) { }
+                String name,
+                String jobId,
+                String scriptId,
+                String scriptCode,
+                String scriptLanguage,
+                String expectedHash,
+                String generatedSdkCode,
+                String generatedSdkHash,
+                Object sourceData) {
+}
