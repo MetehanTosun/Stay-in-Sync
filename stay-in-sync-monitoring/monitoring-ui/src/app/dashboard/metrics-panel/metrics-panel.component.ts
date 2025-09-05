629b713140f9dee07c0761c542fdb9018a1abb62
st187824 <st187824@stud.uni-stuttgart.de>
2025-09-05 19:24:29 +0200

    Sonstiges: testkonflikte

--- stay-in-sync-monitoring/monitoring-ui/src/app/dashboard/metrics-panel/metrics-panel.component.ts
+++ stay-in-sync-monitoring/monitoring-ui/src/app/dashboard/metrics-panel/metrics-panel.component.ts
@@ -8,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class MetricsPanelComponent {
 
+  selectedSyncJobId = '5678';
+  grafanaUrl = `http://grafana.example.com/d/abcd1234/metrics?var-syncJobId=${this.selectedSyncJobId}`;
+
 }
