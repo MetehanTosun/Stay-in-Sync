b8ba1f16d1c7fcf50bcda29d3a23bbc7e85fc176
Max Peresunchak <st152466@stud.uni-stuttgart.de>
2025-10-05 14:48:56 +0200

    Fix add id inside constructor for RequestConfigurationMessageDTO

--- stay-in-sync-core/core-sync-node/src/test/java/de/unistuttgart/stayinsync/transformation/TransformationExecutionIntegrationTest.java
+++ stay-in-sync-core/core-sync-node/src/test/java/de/unistuttgart/stayinsync/transformation/TransformationExecutionIntegrationTest.java
@@ -88,7 +88,7 @@ public class TransformationExecutionIntegrationTest {
 
     private TransformationMessageDTO createTransformationContext(String mockApiBaseUrl) {
         Set<RequestConfigurationMessageDTO> targetArcs = Set.of(
-                new RequestConfigurationMessageDTO("synchronizeProducts", mockApiBaseUrl,
+                new RequestConfigurationMessageDTO(1L,"synchronizeProducts", mockApiBaseUrl,
                         List.of(
                                 new ActionMessageDTO(TargetApiRequestConfigurationActionRole.CHECK, 1, "GET", "/products/search"),
                                 new ActionMessageDTO(TargetApiRequestConfigurationActionRole.CREATE, 2, "POST", "/products"),
