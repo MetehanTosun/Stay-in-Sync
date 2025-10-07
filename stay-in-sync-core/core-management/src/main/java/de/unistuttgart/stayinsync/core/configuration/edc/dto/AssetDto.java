65256fcae57e87cc89940a9c9ed4fc221fa1da3b
spicysesam <st187719@stud.uni-stuttgart.de>
2025-10-07 19:18:19 +0200

    AssetDto updated to support all attributes needed to make complete asset requests to the edc

---
+++ stay-in-sync-core/core-management/src/main/java/de/unistuttgart/stayinsync/core/configuration/edc/dto/AssetDto.java
@@ -0,0 +1,28 @@
+package de.unistuttgart.stayinsync.core.configuration.edc.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonView;
+
+/**
+ * DataTransferObject for EDC Asset communication.
+ * Represents an asset in the Eclipse Dataspace Connector format.
+ *
+ * @param context JSON-LD context, always set to default EDC context
+ * @param type Type identifier, always "Asset"
+ * @param id Unique identifier for the asset (corresponds to asset_id in database)
+ * @param properties Asset metadata (name, description, etc.)
+ * @param dataAddress Configuration for accessing the actual data
+ */
+public record AssetDto(
+        @JsonProperty("@context")
+        AssetContextDto context,
+        @JsonProperty("@type")
+        String type,
+        @JsonProperty("@id")
+        String id,
+        AssetPropertiesDto properties,
+        AssetDataAddressDto dataAddress,
+        @JsonView(VisibilitySidesForDto.Ui.class)
+        Boolean thirdPartyChanges
+) {
+}
