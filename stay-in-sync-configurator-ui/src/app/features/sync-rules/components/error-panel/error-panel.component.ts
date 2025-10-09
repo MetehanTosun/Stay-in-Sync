d99bbf663bd0bbbe60ade06566a916dab8d0105b
st188256 <st188256@stud.uni-stuttgart.de>
2025-10-09 11:06:01 +0200

    Refactors the error panel

--- stay-in-sync-configurator-ui/src/app/features/sync-rules/components/error-panel/error-panel.component.ts
+++ stay-in-sync-configurator-ui/src/app/features/sync-rules/components/error-panel/error-panel.component.ts
@@ -7,13 +7,25 @@ import { ValidationError } from '../../models/interfaces/validation-error.interf
  */
 @Component({
   selector: 'app-error-panel',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './error-panel.component.html',
   styleUrl: './error-panel.component.css'
 })
 export class ErrorPanelComponent {
+  /**
+   * Array of validation errors provided by the backend.
+   */
   @Input() errors: ValidationError[] = [];
+
+  /**
+   * Event emitted when the user selects on an applicable error to focus the canvas view on the contained node id
+   */
   @Output() errorClicked = new EventEmitter<number>();
+
+  /**
+   * Describes the state of the error panel (i.e. expanded or contracted)
+   */
   open = false;
 
   /**
