adbb33c264b26542e30db4c3b124dd63a7bf1fcd
Metehan Tosun <st188557@stud.uni-stuttgart.de>
2025-10-17 00:53:29 +0200

    Corrected imports

--- stay-in-sync-core/core-management/src/main/java/de/unistuttgart/stayinsync/core/configuration/service/transformationrule/TransformationRuleService.java
+++ stay-in-sync-core/core-management/src/main/java/de/unistuttgart/stayinsync/core/configuration/service/transformationrule/TransformationRuleService.java
@@ -15,9 +15,6 @@ import de.unistuttgart.graphengine.validation_error.GraphStatus;
 import de.unistuttgart.graphengine.validation_error.ValidationError;
 import de.unistuttgart.stayinsync.core.configuration.persistence.entities.sync.LogicGraphEntity;
 import de.unistuttgart.stayinsync.core.configuration.persistence.entities.sync.TransformationRule;
-import de.unistuttgart.stayinsync.core.configuration.exception.CoreManagementException; // Import der korrekten Exception
-import de.unistuttgart.stayinsync.core.configuration.domain.entities.sync.LogicGraphEntity;
-import de.unistuttgart.stayinsync.core.configuration.domain.entities.sync.TransformationRule;
 import de.unistuttgart.stayinsync.core.configuration.exception.CoreManagementException;
 import io.quarkus.logging.Log;
 import jakarta.enterprise.context.ApplicationScoped;
